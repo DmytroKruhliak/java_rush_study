a1519008e6d3e171f3547c7abdc0e1ed12bfb280
Kruhliak_Dmytro <46688347+KruhliakDmytro@users.noreply.github.com>
2019-09-13 00:07:15 +0300

    task 0314 is done (multiple tablet)

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
@@ -6,7 +6,19 @@ package com.javarush.task.task03.task0314;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+
+        getMultipleTable(10);
 
     }
+
+    private static void getMultipleTable(int rate) {
+
+        for (int i = 1; i <= rate; i++){
+            for (int j = 1; j <= rate; j++){
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
