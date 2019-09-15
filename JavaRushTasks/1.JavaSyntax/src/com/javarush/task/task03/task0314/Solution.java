521408f1f9d7ae70dd6753ffeeb87adec9b5eadd
Kruhliak_Dmytro <46688347+KruhliakDmytro@users.noreply.github.com>
2019-09-15 23:43:32 +0300

    task 0132 is done (sum of three-digit numbers)

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
@@ -4,8 +4,17 @@ package com.javarush.task.task03.task0314;
 Таблица умножения
 */
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.read());
 
         getMultipleTable(10);
 
