5b040267eb2d906fe74185f8af20ab8210e9a933
Kruhliak_Dmytro <46688347+KruhliakDmytro@users.noreply.github.com>
2019-09-25 00:03:50 +0300

    task with traffic lights was vary interesting !

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task04/task0417/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task04/task0417/Solution.java
@@ -8,21 +8,32 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Solution {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
     public static void main(String[] args) throws Exception {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arrayOfNumbers = new int[3];
 
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
-        int c = Integer.parseInt(br.readLine());
+        for (int i = 0; i < arrayOfNumbers.length; i++) {
+            arrayOfNumbers[i] = Integer.parseInt(br.readLine());
+        }
 
-        //ifEquals();
-
+        ifEquals(arrayOfNumbers[0], arrayOfNumbers[1], arrayOfNumbers[2]);
     }
 
     private static void ifEquals(int a, int b, int c) {
-        if (a == b) {
 
+        if ((a == b) && (a == c)) {
+            System.out.println(a + " " + b + " " + c);
+        } else if (a == b) {
+            System.out.println(a + " " + b);
+        } else if (b == c) {
+            System.out.println(b + " " + c);
+        } else if (c == a) {
+            System.out.println(c + " " + a);
         }
 
     }
