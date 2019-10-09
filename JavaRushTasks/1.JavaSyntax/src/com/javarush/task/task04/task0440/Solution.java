b30201887fddd2f81364dc93de9f37f14263a7eb
Kruhliak_Dmytro <46688347+KruhliakDmytro@users.noreply.github.com>
2019-10-09 23:40:08 +0300

    task 0440 is done, 5th level soon!

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task04/task0440/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task04.task0440;
+
+/* 
+Достойная оплата труда
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int counter = 0;
+        while(true) {
+            System.out.println("Я никогда не буду работать за копейки. Амиго");
+            counter++;
+            if(counter == 100) {
+                break;
+            }
+
+        }
+
+    }
+}
