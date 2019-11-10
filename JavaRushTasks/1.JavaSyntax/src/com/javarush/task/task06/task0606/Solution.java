5fd231d60203d17a167f6b99df84382c300b0ef8
Kruhliak_Dmytro <46688347+KruhliakDmytro@users.noreply.github.com>
2019-11-10 18:39:01 +0200

    task 0606 is done! I have done hard task with 1st trying, and now I feel great!!!

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task06/task0606/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task06.task0606;
+
+import java.io.*;
+
+/* 
+Чётные и нечётные циферки
+*/
+
+public class Solution {
+
+    public static int even;
+    public static int odd;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer inputNumb = new Integer(br.readLine());
+        getCount(inputNumb);
+        System.out.println("Even: " + even + " Odd: " + odd);
+    }
+
+    public static void getCount(int inputNumber) {
+        int temp = inputNumber;
+        String getLength = String.valueOf(inputNumber);
+        int divider = 10;
+
+        for (int i = 0; i <= getLength.length(); i++) {
+            int resByModule = (inputNumber % divider); //divider is increase by order of magnitude every cycle step
+
+            if (resByModule / (divider / 10) % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+            if (resByModule == inputNumber) {
+                break;
+            }
+            divider *= 10;
+        }
+
+    }
+}
