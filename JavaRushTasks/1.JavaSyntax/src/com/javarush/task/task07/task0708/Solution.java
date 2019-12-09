abbd1b6f2a54385c243a17092bbc76130d2e7a47
Kruhliak_Dmytro <46688347+KruhliakDmytro@users.noreply.github.com>
2019-12-09 21:18:28 +0200

    task 0708 is done, I created couple of methods that doing decomposed work, one method fill an array, second one finds longest string and return their size (int) and third find equaled length string and sout then in the console.

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0708/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0708/Solution.java
@@ -1,12 +1,10 @@
 package com.javarush.task.task07.task0708;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 /* 
 Самая длинная строка
@@ -16,21 +14,53 @@ public class Solution {
     private static ArrayList<String> strings;
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш
-        ArrayList<String> arrayList = new ArrayList<>();
-        initArrayList(arrayList);
+        //напишите тут ваш код
 
+        strings = new ArrayList<>(5);
 
-        System.out.println(Arrays.toString(arrayList.toArray()));
+        initArrayList(strings); //this method returns filled ArrayList by BufferedReader
+
+        showLongestStrings(strings);
+
 
     }
 
-    public static void initArrayList(List<String> list) throws Exception {
+    public static void initArrayList(List<String> list) throws Exception { // fil the ArrayLIst
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int i = 0;
-        while(5 > i) {
+        while (5 > i) {
             list.add(br.readLine());
             i++;
         }
+    }
+
+    public static void showLongestStrings(ArrayList<String> list) {
+        int pivotLength = getLongestString(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            String sTemp = list.get(i); // temporary container for String from the list
+
+            if (pivotLength == sTemp.length()) {
+                System.out.println(sTemp);
+            }
+
+
+
+        }
+    }
+
+
+    public static int getLongestString(ArrayList<String> list) { //this method returns count of characters of the longest String in Array
+        int sLengthTemp = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            String sTemp = list.get(i);
+
+            if (sTemp.length() > sLengthTemp) {
+                sLengthTemp = sTemp.length();
+            }
+        }
+
+        return sLengthTemp;
     }
 }
