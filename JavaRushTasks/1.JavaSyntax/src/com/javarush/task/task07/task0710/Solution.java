b5a729a69d1025603a7ec3deedbec8d5cc2eb7da
Kruhliak_Dmytro <46688347+KruhliakDmytro@users.noreply.github.com>
2019-12-15 21:40:43 +0200

    sometimes you need to make a wind(its mean - get some drunk) and then back to solving task and exercises, lol - task 0710 is done

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0710/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task07.task0710;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/* 
+В начало списка
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> strList = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (strList.size() != 10)
+            strList.add(0, br.readLine());
+
+        for (String s : strList) {
+            System.out.println(s);
+        }
+    }
+}
