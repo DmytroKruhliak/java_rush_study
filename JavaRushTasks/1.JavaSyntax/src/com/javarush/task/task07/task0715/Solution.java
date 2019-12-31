63bc86acc123116ed8af3baba0408a5c23189f98
Kruhliak_Dmytro <46688347+KruhliakDmytro@users.noreply.github.com>
2019-12-31 17:21:09 +0200

    New Year commit! task 0715 is done!

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0715/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task07.task0715;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Продолжаем мыть раму
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        ArrayList<String> list = new ArrayList<>();
+
+        exactlyAfter(list, "мама");
+        exactlyAfter(list, "мыла");
+        exactlyAfter(list, "раму");
+
+        printList(list);
+
+    }
+
+    public static void exactlyAfter(ArrayList<String> list, String s) {
+        list.add(s);
+        list.add("именно");
+    }
+
+    public static void printList(ArrayList<String> list) {
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+    }
+}
