599878efefa135e4796715a6592511dc9d5e0fc2
Kruhliak_Dmytro <46688347+KruhliakDmytro@users.noreply.github.com>
2020-01-02 20:55:59 +0200

    continue making task 0716

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
@@ -12,6 +12,7 @@ public class Solution {
         strings.add("роза");
         strings.add("лоза");
         strings.add("лира");
+
         strings = fix(strings);
 
         for (String string : strings) {
@@ -21,6 +22,41 @@ public class Solution {
 
     public static ArrayList<String> fix(ArrayList<String> strings) {
         //напишите тут ваш код
-        return null;
+        int i = 0;
+        for (String s : strings) {
+            if (containsL(s) && containsR(s)) {
+                i++;
+            } else if (containsR(s)) {
+                strings.remove(s);
+            } else if (containsL(s)){
+                strings.add(s);
+            }
+        }
+        return strings;
     }
+
+    public static boolean containsR(String s) {
+        char[] charsArr = s.toCharArray();
+        boolean result = false;
+        for (char c : charsArr) {
+            if (c == 'р') {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean containsL(String s) {
+        char[] charsArr = s.toCharArray();
+        boolean result = true;
+        for (char c : charsArr) {
+            if (c == 'л') {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
 }
